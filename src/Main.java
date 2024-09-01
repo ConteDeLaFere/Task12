eae80db7e3335c64a46f1777234afee77f969298
Nikita Malinkin <nik.malina20@gmail.com>
2024-09-01 18:12:51 +0300

    Task solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> books = new ArrayList<>(List.of("Harry Potter", "Lord of the Rings", "Hobbit", "War and Peace", "Crime and Punishment",
+                "Pride and Prejudice", "To Kill a Mockingbird", "The Great Gatsby", "Moby Dick", "Ulysses", "Raven", "Sorrow", "Fun", "Love", "Weak", "Strike", "Captain's daughter"));
+
+        int shelvesCount = 7;
+
+        List<List<String>> list = Task12.booksOnShelves(books, shelvesCount);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Полка " + (i + 1) + " " + list.get(i));
+        }
     }
 }
